ec3abb19b6da80c668939f108f1cf8d9054b8cbc
Alexander Brown <alexander.brown@binarycomputer.co.uk>
2016-11-15 16:20:48 +0000

    junit build added

--- binary1/src/BinaryMain.java
+++ binary1/src/BinaryMain.java
@@ -2,7 +2,6 @@
 public class BinaryMain {
 
 	public static void main(String[] args){
-		System.out.println(args[0]);
 		System.out.println("hi guys!");
 		System.out.println("hey");
 		System.out.println("test5");
---
+++ binary1/src/test/LotteryTest.java
@@ -0,0 +1,30 @@
+package test;
+import static org.junit.Assert.*;
+import junit.framework.Assert;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class LotteryTest {
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		Assert.assertEquals(1,1);
+	}
+
+	@Test
+	public void test2() {
+		Assert.assertEquals(1,1);
+	}
+}
