598e4084f8fbf42ec46d012c279442bed28c53b2
Alexander Brown <alexander.brown@binarycomputer.co.uk>
2016-11-16 15:48:07 +0000

    to correct semicolon issue

--- binary1/src/main/java/binary1/BinaryMain.java
+++ binary1/src/main/java/binary1/BinaryMain.java
@@ -7,7 +7,7 @@ public class BinaryMain {
 	 * @param args
 	 */
 	public static void main(String[] args){
-		System.out.println("This is working the way I want.")
+		System.out.println("This is working the way I want.");
 		}
 	
 
