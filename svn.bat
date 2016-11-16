db314740f64a393fddf83318b67d1be0754c8520
Alexander Brown <alexander.brown@binarycomputer.co.uk>
2016-11-16 15:29:58 +0000

    svn starts cmd in its own process and should stop loops

--- svn.bat
+++ svn.bat
@@ -1,4 +1,4 @@
-cd C:\Users\Alexander\Documents\RTC\workspace\svnproject\trunk
-git pull origin master
-svn add *
-svn commit -m "commitmade"
+echo "released trunk"
+start cmd /c C:\Users\Alexander\Documents\RTC\workspace\svnproject\trunk & git pull origin master & svn add * & svn commit -m "commitmade"
+
+pause
