eaf233ce940a47533765aafa6c36eb8190924ae6
Alexander Brown <alexander.brown@binarycomputer.co.uk>
2016-11-16 16:06:06 +0000

    sub directory of binary 1 is moved outside parent

--- src/main/java/binary1/BinaryMain.java
+++ src/main/java/binary1/BinaryMain.java
@@ -8,7 +8,15 @@ public class BinaryMain {
 	 */
 	public static void main(String[] args){
 		System.out.println("This is working the way I want.");
+		
+		while(true){
+			System.out.println("Do something");
+			
 		}
+		System.out.println("Do something");
+	}
 	
 
 }
+
+
