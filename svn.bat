795384d0d6032f8ee81f2373649055118d0daeb6
Alexander Brown <alexander.brown@binarycomputer.co.uk>
2016-11-16 15:38:19 +0000

    svn.bat has looping issues so renamed to svn2.bat

--- svn.bat
+++
@@ -1,4 +0,0 @@
-echo "released trunk"
-start cmd /c C:\Users\Alexander\Documents\RTC\workspace\svnproject\trunk & git pull origin master & svn add * & svn commit -m "commitmade"
-
-pause
---
+++ svn2.bat
@@ -0,0 +1,6 @@
+echo "released trunk"
+C:\Users\Alexander\Documents\RTC\workspace\svnproject\trunk
+git pull origin master
+svn add * & svn commit -m "commitmade"
+
+EXIT
