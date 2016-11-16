5ea2670f662daea52edddd4a6d935ec9940f3102
Alexander Brown <alexander.brown@binarycomputer.co.uk>
2016-11-16 15:41:11 +0000

    changes to make bat work in jenkins

--- svn2.bat
+++ svn2.bat
@@ -1,6 +1,7 @@
 echo "released trunk"
-C:\Users\Alexander\Documents\RTC\workspace\svnproject\trunk
+cd C:\Users\Alexander\Documents\RTC\workspace\svnproject\trunk
 git pull origin master
-svn add * & svn commit -m "commitmade"
+svn add *
+svn commit -m "commitmade"
 
 EXIT
